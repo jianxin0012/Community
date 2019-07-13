986e73bf243faea9d9f3f783f11eb176cb8ea8a9
jianxin0012 <641484317@qq.com>
2019-07-13 17:40:16 +0800

    登录持久化

--- src/main/java/community/community/mapper/UserMapper.java
+++ src/main/java/community/community/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package community.community.mapper;
 import community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (account_id,name,token,create_time,modified_time) values(#{accountId},#{name},#{token},#{create_time},#{modified_time})")
     void insert(User user);
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
