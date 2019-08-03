33a070e273c8fe6811704def9ecff1a2bdaf8525
jianxin0012 <641484317@qq.com>
2019-08-03 15:19:44 +0800

    flyway

---
+++ src/main/resources/db/migration/V1__Create_Test_table.sql
@@ -0,0 +1,10 @@
+create table test2
+(
+  id            int auto_increment
+    primary key,
+  account_id    varchar(100) null,
+  name          varchar(50)  null,
+  token         char(36)     null,
+  create_time   bigint       null,
+  modified_time bigint       null
+);
